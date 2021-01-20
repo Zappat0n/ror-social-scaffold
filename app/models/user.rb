79d4e1612d223128087ff11bcd6742be3b3c834c
angel <albarros1973@gmail.com>
2021-01-20 18:04:19 +0100

    Fix problems with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,15 +21,19 @@ class User < ApplicationRecord
   def pending_friends
     friendships.map{|friendship| friendship.friend if !friendship.status}.compact
   end
+
   def friend_requests
     inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
   end
+
   def confiirm_friend(user)
     friendship = inverse_friendships.find{|friendship| friendship.user == user}
     friendship.status = true
     friendship.save
   end
+
   def friend?(user)
     friends.include?(user)
   end
+
 end
--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.feature "CreatePosts", type: :feature do
   before :each do
     @user = User.create(name: 'Peter', email: 'x@x.com', password: '123456')
+    sign_in @user
     @post = Post.new(user: @user, content: 'Hello!')
     visit root_path
   end
 
-#  it 'user can create a post' do
-#    fill_in 'Content',	with: @post.content
-#    click_button 'Save'
-#    expect(current_path).to eq(root_path)
-#    expect(page).to have_text('Post was successfully created.')
-#  end
+  it 'user can create a post' do
+    fill_in 'post_content', with: @post.content
+    click_button 'Save'
+    expect(current_path).to eq(posts_path)
+    expect(page).to have_text('Post was successfully created.')
+  end
 end
