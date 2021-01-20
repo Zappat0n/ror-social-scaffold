9fd31c8db3f7e6967b04f064fa839c517c638f82
angel <albarros1973@gmail.com>
2021-01-20 14:36:57 +0100

    Updade rspec feature create comment

--- spec/features/create_comment_spec.rb
+++ spec/features/create_comment_spec.rb
@@ -4,11 +4,11 @@ RSpec.feature "CreateComments", type: :feature do
   before :each do
     @user = User.create(name: 'Peter', email: 'x@x.com', password: '123456')
     @post = Post.create(user: @user, content: 'Hello!')
-    visit root
+    visit root_path
   end
 
   it 'user can create a comment' do
-    fill_in :form-control,	with: 'I like your post'
+    fill_in 'comment_content',	with: 'I like your post'
     click_button 'COMMENT'
     expect(current_path).to eq(root)
     expect(page).to have_text('Comment was successfully created.')
