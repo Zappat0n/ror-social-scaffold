cd8db5cd51e9279f77faca88cf0a686538a32301
Nick Kuria <nkuria01@gmail.com>
2021-01-25 12:19:53 +0300

    removed id column from friendships and used ckp to create pk relationship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  self.primary_keys = :user_id, :friend_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_095305) do
+ActiveRecord::Schema.define(version: 2021_01_25_091651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_01_21_095305) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigserial "id", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
